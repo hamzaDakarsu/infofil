30e5eb4f336f46ededfd044f37dd0b51c49503ee
hamzaDakarsu <hamzad.teacher@gmail.com>
2022-12-18 22:57:27 +0300

    pls

--- src/comps/SearchBox.js
+++ src/comps/SearchBox.js
@@ -7,7 +7,7 @@ const SearchBox = (props) => {
 				className='form-control'
 				value={props.value}
 				onChange={(event) => props.setSearchValue(event.target.value)}
-				placeholder='Filim Arayınız...'
+				placeholder='Film Arayınız...'
 			></input>
 		</div>
 	);
